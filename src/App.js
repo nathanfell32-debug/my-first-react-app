d5a2f5784b8e27a7628e974f0164edb15edc1fb0
nathanfell32-debug <nathan.fell32@gmail.com>
2026-08-11 12:10:31 +0100

    activity 16 - enhancing the job management application with reusable components
    Objective:
    
    Refactor the existing job management application to use more reusable components, implement props for dynamic content, and create a flexible layout for job status columns.
    activity 16 carry's on from activity 14

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from "react";
 import { useState } from "react";
-import JobForm from "./activity-14/JobForm";
+import JobForm from "./activity-14-15-16/JobForm";
+import JobColumn from "./activity-14-15-16/JobColumn";
+import startIcon from "./activity-14-15-16/icons8-play-button-48.png";
+import progressIcon from "./activity-14-15-16/icons8-in-progress-50.png";
+import completeIcon from "./activity-14-15-16/icons8-task-completed-48.png";
 
 function App() {
   const [jobs, setJobs] = useState([]);
+
+  const needToStart = jobs.filter(job => job.status === "start");
+  const inProgress = jobs.filter(job => job.status === "in-progress");
+  const completed = jobs.filter(job => job.status === "completed");
 
   function handleAddJob(newJob) {
     setJobs([...jobs, newJob]);
@@ -14,15 +22,24 @@ function App() {
       <h1>Job Management App</h1>
 
       <JobForm onAddJob={handleAddJob} />
-
-      <h2>Jobs Added:</h2>
-      <ul>
-        {jobs.map((job, index) => (
-          <li key={index}>
-            <strong>{job.title}</strong>> - {job.company}, {job.location}
-          </li>
-        ))}
-      </ul>
+      
+      <div className="column-container">
+        <JobColumn
+        title="Need to start"
+        image={startIcon}
+        jobs={needToStart}
+      />
+        <JobColumn
+        title="In Progress"
+        image={progressIcon}
+        jobs={inProgress}
+      />
+        <JobColumn
+        title="Completed"
+        image={completeIcon}
+        jobs={completed}
+      />
+      </div>
     </div>
   );
 }
