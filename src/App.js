ba769caa6532f287a4f9523936addd9206a6b6dc
nathanfell32-debug <nathan.fell32@gmail.com>
2026-08-14 16:24:10 +0100

    Practical Activity 23- Implement Local Storage Persistence with useEffect in a React Job Management App
    Objective: Enhance the Job Management application by implementing local storage persistence using the useEffect hook, ensuring that jobs are saved and retrieved even after page refreshes.
    
    this activity continues on from activity 14 -22

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import JobManager from "./activity-14-15-16-17-18-19-20-21-22/JobManager";
+import JobManager from "./activity-14-15-16-17-18-19-20-21-22-23/JobManager";
 
 function App() {
   return <JobManager />
--- src/activity-14-15-16-17-18-19-20-21-22-23/JobManager.jsx
+++ src/activity-14-15-16-17-18-19-20-21-22-23/JobManager.jsx
@@ -1,15 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import JobForm from "./JobForm";
 import JobColumn from "./JobColumn";
 
+//load jobs from localStorage on first render
 function JobManager() {
-    const [jobs, setJobs] = useState([]);
+    const [jobs, setJobs] = useState(() => {
+        const savedJobs = localStorage.getItem("jobs");
+        return savedJobs ? JSON.parse(savedJobs) : [];
+    });
 
+    //save jobs to localStorage whenever jobs change
+    useEffect(() => {
+        localStorage.setItem("jobs", JSON.stringify(jobs));
+    }, [jobs]);
+// add a new job
     function addJob(newJob) {
         setJobs([...jobs, newJob]);
     }
+    //delete a job
     function deleteJob(jobId) {
         setJobs(jobs.filter(job => job.id !== jobId));
+    }
+    // clear all jobs
+    function clearAllJobs() {
+        setJobs([]);
+        localStorage.removeItem("jobs");
     }
 
     return (
@@ -22,6 +37,17 @@ function JobManager() {
             <h1 style={{ textAlign: "center" }}>Job Management UI</h1>
 
             <JobForm onAddJob={addJob} />
+            <button
+            onClick={clearAllJobs}
+            style={{
+                marginTop: "10px",
+                padding: "10px",
+                backgroundColor: "darkred",
+                color: "white",
+                border: "none",
+                borderRadius: "4px",
+                cursor: "pointer"
+            }}>Clear All Jobs</button>
 
             <div style={{
                 display: "flex",
